8a6d878f9c1cead9fcdda1d6f0bbed17ce4e488a
Luis Rodriguez <webfacilco@gmail.com>
2019-11-29 15:37:26 -0500

    Categories UI finished

--- src/store/index.js
+++ src/store/index.js
@@ -5,26 +5,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    urlApi: 'https://api.tissini.app',
     categories: [],
     sections: []
   },
   mutations: {
     showCategories (state, categoriesActions) {
+      // Push data to state
       state.categories = categoriesActions;
     },
     showSections (state, sectionsActions) {
+      // Push data to state
       state.sections = sectionsActions;
     }
   },
   actions: {
     getCategories: async function ({ commit }) {
-      const urlApi = 'https://api.tissini.app/api';
       // Get categories
-      const dataCategories = await fetch(urlApi + '/v2/categories');
+      const dataCategories = await fetch(this.state.urlApi + '/api/v2/categories');
       const categories = await dataCategories.json();
+      // Sort by pos attribute
+      categories.sort((a, b) => (a.pos > b.pos) ? 1 : -1);
+
       // Get sections
-      const dataSections = await fetch(urlApi + '/v1/categories/sections');
+      const dataSections = await fetch(this.state.urlApi + '/api/v1/categories/sections');
       const sections = await dataSections.json();
+
       // Commit to mutations
       commit('showCategories', categories);
       commit('showSections', sections);
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,40 +1,190 @@
 <template>
   <main class="categories">
     <Navbar />
-    <ul class="categories">
-      <li v-for="(category, i) of categories" :key="i">
-        {{category.name}}
-      </li>
-    </ul>
+    <div class="container">
+      <div class="content text-center">
+        <div class="banner-top">
+          <img :src="bannerTopUri" alt="Productos de entrega inmediata">
+          <router-link v-bind:to="'/category/'" class="btn btn-sm btn-dark">Ver más</router-link>
+        </div>
 
-    <ul class="sections">
-      <li v-for="(section, i) of sections" :key="i">
-        {{section.name}}
-      </li>
-    </ul>
+        <h3 class="text-left">Líneas</h3>
+
+        <ul class="categories">
+          <li v-for="(category, i) of categories" :key="i" v-show="category.active == 1" class="category">
+            <router-link v-bind:to="'/category/' + category.id">
+              <img :src="urlApi + category.image" :alt="category.name">
+              <button class="btn btn-sm btn-dark">Ver más</button>
+            </router-link>
+          </li>
+        </ul>
+
+        <ul class="sections">
+          <li v-for="(section, i) of sections" :key="i" class="section">
+            <img :src="urlApi + section.image" :alt="section.name" class="banner">
+            <ul class="products clearfix">
+              <li v-for="(product, i) of section.products" :key="i" class="product">
+                <router-link v-bind:to="'/category/products/' + product.id">
+                  <img :src="urlApi + product.image.url" :alt="product.name" class="img">
+                  <p class="category text-left text-uppercase">{{ product.categories.category }}</p>
+                  <p class="name text-left">{{ product.name }}</p>
+                  <p class="price text-left">${{ product.price }}</p>
+                </router-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import Vuex from 'vuex';
 import Navbar from '@/components/Navbar.vue';
-import Vuex from 'vuex'
 
 export default {
   name: 'categories',
   components: {
     Navbar
   },
+  data() {
+    return {
+      bannerTopUri: 'https://mitienda.moda/img/productos-de-entrega-inmediata.e07d1e57.png'
+    }
+  },
   created() {
+    // Execute actions for get data from API
     this.$store.dispatch('getCategories', 'getSections');
   },
   computed: {
-    ...Vuex.mapState(['categories', 'sections'])
+    // Get state from store
+    ...Vuex.mapState(['categories', 'sections', 'urlApi']),
   },
   methods: {
+    // Get actions from store
     ...Vuex.mapActions(['getCategories', 'getSections'])
   }
 }
+
 </script>
 
 <style scoped lang="scss">
+  $pink: #f06292;
+  $gray: #aaa;
+  $black: #555;
+
+  .content {
+    margin: 0 auto;
+    width: 350px;
+
+    @media (max-width: 370px) {
+      width: 100%;
+    }
+
+    .banner-top {
+      margin-bottom: 15px;
+      position: relative;
+
+      img {
+        max-width: 350px;
+          width: 100%;
+      }
+
+      a {
+        bottom: 5px;
+        position: absolute;
+        right: 5px;
+      }
+    }
+
+    h3 {
+      color: $gray;
+      font-weight: 300;
+    }
+
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+
+      &.categories {
+
+        .category {
+          cursor: pointer;
+          margin-bottom: 10px;
+          position: relative;
+
+          img {
+            max-width: 350px;
+            width: 100%;
+          }
+
+          button {
+            bottom: 12px;
+            position: absolute;
+            right: 12px;
+          }
+        }
+      }
+
+      &.sections {
+        .banner {
+          max-width: 350px;
+          width: 100%;
+        }
+
+        .section {
+          margin-top: 30px;
+
+          .products {
+            margin-top: 20px;
+
+            .product {
+              border: 1px solid #e9e9e9;
+              border-radius: 5px 5px 0 0;
+              float: left;
+              margin-bottom: 20px;
+              padding: 2%;
+              width: 48%;
+
+              &:nth-child(odd) {
+                margin-right: 4%;
+              }
+
+              .img {
+                border-radius: 5px 5px 0 0;
+                margin-bottom: 10px;
+                max-width: 100%;
+                width: 100%;
+              }
+
+              p {
+                line-height: normal;
+                margin: 0px;
+
+                &.category {
+                  color: $gray;
+                  font-size: 13px;
+                }
+
+                &.name {
+                  color: $black;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                  white-space: nowrap;
+                }
+
+                &.price {
+                  color: $pink;
+                  font-size: 15px;
+                  margin-top: 2px;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
